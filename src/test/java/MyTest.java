c921700c641bb88785346c63692d56bd7614fc73
Mirad <miradbk@bk.ru>
2024-03-20 16:59:59 +0300

    init_homework1

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+    @Test
+    public void uselessTest(){
+        Assertions.assertTrue(true);
+    }
+}
